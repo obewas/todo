696bd40af84c39f536c07b7f1eca063430e53053
obewas <obewas1202@gmail.com>
2022-08-30 20:00:53 +0300

    added item component

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
+import { Item } from "../item";
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
 
-  constructor() { }
+export class ItemComponent {
 
-  ngOnInit(): void {
+  editable = false;
+
+  @Input() item!: Item;
+  @Input() newItem!: string;
+  @Output() remove = new EventEmitter<Item>();
+
+  saveItem(description:any) {
+    if (!description) return;
+    this.editable = false;
+    this.item.description = description;
   }
-
 }
+
